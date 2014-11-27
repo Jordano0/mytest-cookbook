4a4394bc9522262e77031f2e0349a1e027aa408a
jarek <jaroslawgodlewski0000@gmail.com>
2014-11-27 14:20:18 +0100

    first commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'mytest-cookbook'
+maintainer 'jgodlewski'
+maintainer_email 'jaroslawgodlewski0000@gmail.com'
+license 'All rights reserved'
+description 'Installs/Configures mytest-cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.0'
