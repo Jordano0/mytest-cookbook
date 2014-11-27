84dd3e6704eb27aa97de71b484caba4eaf7829f9
jarek <jaroslawgodlewski0000@gmail.com>
2014-11-27 15:03:12 +0100

    recipe update

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,39 +6,19 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-package 'httpd' do
-  action :install
+#
+# Cookbook Name:: myapache-cookbook
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+package node['myapache-cookbook']['package'] do
+action :install
 end
-execute 'mv /etc/httpd/conf.d/welcome.conf /etc/httpd/conf.d/welcome.conf.bak' do
-  only_if do
-    File.exist?('/etc/httpd/conf.d/welcome.conf')
-  end
-  notifies :restart, 'service[httpd]'
+service node['myapache-cookbook']['package'] do
+action [:enable, :start]
 end
-node['apache']['sites'].each do |site_name, site_data|
-  document_root = "/srv/apache/#{site_name}"
-  template "/etc/httpd/conf.d/#{site_name}.conf" do
-    source 'custom.erb'
-    mode '0644'
-    variables(
-      document_root => document_root,
-      port => site_data['port']
-    )
-    notifies :restart, 'service[httpd]'
-  end
-  directory document_root do
-    mode '0755'
-    recursive true
-  end
-  template "#{document_root}/index.html" do
-    source 'index.html.erb'
-    mode '0644'
-    variables(
-    site_name => site_name,
-    port => site_data['port']
-    )
-  end
-end
-service 'httpd' do
-  action [:enable, :start]
+service 'iptables' do
+action [:disable, :stop]
 end
